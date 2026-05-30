fec9000ace168d77dc34b3a9ef84fab88e714911
Hyunsoo_kim <red_panda119@naver.com>
2026-05-30 17:47:59 +0900

    feat : Header 렌더링

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { notoSansKr, notoSerifKr } from "./fonts";
 
 import './globals.scss';
+import { Header } from '@/components/layouts/Header/Header';
 
 export const metadata: Metadata = {
   title: '전국문화축제',
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko' className={`${notoSansKr.variable} ${notoSerifKr.variable}`}>
-      <body>{children}전국문화축제</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
