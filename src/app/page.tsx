2fd53f779ae7c60f0d393d35e0fdbb1592fd238e
Hyunsoo_kim <red_panda119@naver.com>
2026-06-01 19:28:28 +0900

    feat : HomeSection 컴포넌트 렌더링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,44 @@
+import { QueryClient, dehydrate, HydrationBoundary } from '@tanstack/react-query';
+
+import { QUERY_KEYS } from '@/constants/queryKeys';
+import { REALMS } from '@/constants/realms';
+import { getAreas } from '@/services/getAreas';
+import { getRealms } from '@/services/getRealms';
 import { HeroSection } from '@/components/sections/HeroSection/HeroSection';
 import { SectionDivider } from '@/components/ui/SectionDivider/SectionDivider';
+import { HomeSection } from '@/components/sections/HomeSection/HomeSection';
 
 import styles from './page.module.scss';
 
-export default function Home() {
+export default async function Home() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: QUERY_KEYS.areas(),
+    queryFn: () => getAreas('전체'),
+  });
+
+  await queryClient.prefetchQuery({
+    queryKey: QUERY_KEYS.realms(),
+    queryFn: () => getRealms(REALMS[0].code),
+  });
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <HeroSection />
-        <SectionDivider label='AREA' />
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <HeroSection />
+          <SectionDivider label='AREA' />
+          <HomeSection
+            category='area'
+            title='지역별 축제'
+          />
+          <SectionDivider label='REALM' />
+          <HomeSection
+            category='realm'
+            title='분야별 축제'
+          />
+        </HydrationBoundary>
       </main>
     </div>
   );
