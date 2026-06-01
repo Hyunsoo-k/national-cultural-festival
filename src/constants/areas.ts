ecd1fa835d2dde04fbc780e227460bf79d715db3
Hyunsoo_kim <red_panda119@naver.com>
2026-06-01 19:25:21 +0900

    feat : areas, realms 상수 선언

---
+++ src/constants/areas.ts
@@ -0,0 +1,13 @@
+export const AREAS = [
+  { code: '전체', name: '전체' },
+  { code: '서울', name: '서울' },
+  { code: '경기', name: '경기' },
+  { code: '강원', name: '강원' },
+  { code: '충북', name: '충북' },
+  { code: '충남', name: '충남' },
+  { code: '전북', name: '전북' },
+  { code: '전남', name: '전남' },
+  { code: '경북', name: '경북' },
+  { code: '경남', name: '경남' },
+  { code: '제주', name: '제주' }
+] as const;
---
+++ src/constants/realms.ts
@@ -0,0 +1,13 @@
+export const REALMS = [
+  { code: 'F000', name: '행사/축제' },
+  { code: 'A000', name: '연극' },
+  { code: 'B002', name: '음악/콘서트' },
+  { code: 'C000', name: '무용/발레' },
+  { code: 'D000', name: '전시' },
+  { code: 'B003', name: '뮤지컬/오페라' },
+  { code: 'E000', name: '아동/가족' },
+  { code: 'G000', name: '교육/체험' },
+  { code: 'H000', name: '도서' },
+  { code: 'I000', name: '체육' },
+  { code: 'L000', name: '기타' }
+] as const;
