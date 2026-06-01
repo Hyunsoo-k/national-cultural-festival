ae6022a53a75d9411ce68c5efb8bf10fcf2f5fd0
Hyunsoo_kim <red_panda119@naver.com>
2026-06-02 01:24:11 +0900

    feat : 쿼리키에 detail 추가

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -1,4 +1,5 @@
 export const QUERY_KEYS = {
   areas: (code?: string) => ['area', { code }] as const,
-  realms: (code?: string) => ['realms', { code }] as const
+  realms: (code?: string) => ['realms', { code }] as const,
+  detail: (seq: string) => ['detail', seq] as const
 };
