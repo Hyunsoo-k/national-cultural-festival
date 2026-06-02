8a24c440c6ebfce34b827bab8563b449988375ed
Hyunsoo_kim <red_panda119@naver.com>
2026-06-03 04:36:11 +0900

    feat : ScreenPaddingWrapper 사용

--- src/containers/ItemsPageContainer/ItemsPageContainer.tsx
+++ src/containers/ItemsPageContainer/ItemsPageContainer.tsx
@@ -1,19 +1,20 @@
 'use client';
 
+import Link from 'next/link';
 import { useState } from 'react';
 
+import { Category } from '@/types/category';
+import { AreaOrRealmItem } from '@/types/areaOrRealmItem';
 import { AREAS } from '@/constants/areas';
 import { REALMS } from '@/constants/realms';
 import { useGetAreas } from '@/hooks/useGetAreas';
 import { useGetRealms } from '@/hooks/useGetRealms';
+import { ScreenPaddingWrapper } from '@/components/layouts/ScreenPaddingWrapper/ScreenPaddingWrapper';
+import { SectionDivider } from '@/components/ui/SectionDivider/SectionDivider';
 import { FilterBar } from '@/components/FilterBar/FilterBar';
+import { Card } from '@/components/Card/Card';
 
 import styles from './ItemsPageContainer.module.scss';
-import Link from 'next/link';
-import { AreaOrRealmItem } from '@/types/areaOrRealmItem';
-import { Category } from '@/types/category';
-import { Card } from '@/components/Card/Card';
-import { SectionDivider } from '@/components/ui/SectionDivider/SectionDivider';
 
 const AREAS_VALUES = Object.keys(AREAS);
 const REALMS_VALUES = Object.keys(REALMS);
@@ -34,11 +35,9 @@ export const ItemsPageContainer = ({ category }: Props) => {
 
   const items = areasData?.body.items.item || reamlsData?.body.items.item;
 
-  console.log(items)
-
   const filterBarItems = category === 'area' ? AREAS_VALUES : REALMS_VALUES;
 
-  const handleCategoryClick = (category: string) => {
+  const handleCategoryClick = (category: Category) => {
     console.log(category);
     setSelectedCategory(category);
   };
@@ -48,36 +47,38 @@ export const ItemsPageContainer = ({ category }: Props) => {
   };
 
   return (
-    <div className={styles.itemsPageContainer}>
-      <div className={styles.categories}>
-        <Link
-          href='/items?category=area'
-          onClick={() => handleCategoryClick('area')}
-          className={`${styles.category} ${selectedCategory === 'area' ? styles.selected : ''}`}
-        >
-          지역별
-        </Link>
-        <Link
-          href='/items?category=realm'
-          onClick={() => handleCategoryClick('realm')}
-          className={`${styles.category} ${selectedCategory === 'realm' ? styles.selected : ''}`}
-        >
-          분야별
-        </Link>
-      </div>
-      <FilterBar
-        items={filterBarItems}
-        selectedFilter={selectedFilter}
-        onClick={handleFilterClick}
-      />
-      <SectionDivider label={category.toUpperCase()} />
-      <ul className={styles.list}>
-        {items?.map((item: AreaOrRealmItem) => (
-          <li key={item.title} className={styles.item}>
-            <Card item={item} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ScreenPaddingWrapper>
+      <main className={styles.itemsPageContainer}>
+        <div className={styles.categories}>
+          <Link
+            href='/items?category=area'
+            onClick={() => handleCategoryClick('area')}
+            className={`${styles.category} ${selectedCategory === 'area' ? styles.selected : ''}`}
+          >
+            지역별
+          </Link>
+          <Link
+            href='/items?category=realm'
+            onClick={() => handleCategoryClick('realm')}
+            className={`${styles.category} ${selectedCategory === 'realm' ? styles.selected : ''}`}
+          >
+            분야별
+          </Link>
+        </div>
+        <FilterBar
+          items={filterBarItems}
+          selectedFilter={selectedFilter}
+          onClick={handleFilterClick}
+        />
+        <SectionDivider label={category.toUpperCase()} />
+        <ul className={styles.list}>
+          {items?.map((item: AreaOrRealmItem) => (
+            <li key={item.title} className={styles.item}>
+              <Card item={item} />
+            </li>
+          ))}
+        </ul>
+      </main>
+    </ScreenPaddingWrapper>
   );
 };
