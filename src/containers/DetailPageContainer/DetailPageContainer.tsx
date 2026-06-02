2827a202399dae413f5b9490dcbb04146051041d
Hyunsoo_kim <red_panda119@naver.com>
2026-06-03 04:42:38 +0900

    feat : ScreenPaddingWrapper 적용

--- src/containers/DetailPageContainer/DetailPageContainer.tsx
+++ src/containers/DetailPageContainer/DetailPageContainer.tsx
@@ -17,6 +17,7 @@ import { KakaoMap } from './components/KakaoMap/KakaoMap';
 import { Slider } from '@/components/Slider/Slider';
 
 import styles from './DetailPageContainer.module.scss';
+import { ScreenPaddingWrapper } from '@/components/layouts/ScreenPaddingWrapper/ScreenPaddingWrapper';
 
 type Props = {
   seq: string;
@@ -42,66 +43,68 @@ export const DetailPageContainer = ({ seq, realm, gpsX, gpsY }: Props) => {
   return (
     <div className={styles.detailPageContainer}>
       <DetailHeroSection item={item} />
-      <section className={styles.section}>
-        <DetailSectionHeader label='DETAILS' title='상세 정보' />
-        <div className={styles.body}>
-          <ul className={styles.infoList}>
-            <li className={styles.item}>
-              <span className={styles.itemLabel}>
-                <CiLocationOn className={styles.icon} />
-                주소
-              </span>
-              <span className={styles.text}>{item.area} {item.place}</span>
-            </li>
-            <li className={styles.item}>
-              <span className={styles.itemLabel}>
-                <PiCalendarMinusThin className={styles.icon} />
-                기간
-              </span>
-              <span className={styles.text}>{formatDate(item.startDate)} ~ {formatDate(item.endDate)}</span>
-            </li>
-            <li className={styles.item}>
-              <span className={styles.itemLabel}>
-                <LiaWonSignSolid className={styles.icon} />
-                가격
-              </span>
-              <span className={styles.text}>{item.price.trim() || '홈페이지 확인'}</span>
-            </li>
-            <li className={styles.item}>
-              <span className={styles.itemLabel}>
-                <PiPhoneCallLight className={styles.icon} />
-                문의
-              </span>
-              <span className={styles.text}>{item.phone}</span>
-            </li>
-            <li className={styles.item}>
-              <span className={styles.itemLabel}>
-                <RiHome2Line className={styles.icon} />
-                홈페이지
-              </span>
-              <span className={styles.text}>
-                <Link
-                  href={decodeHtml(item.url)}
-                  target='_blank'
-                  rel="noopener noreferrer"
-                  >
-                    {decodeHtml(item.url)}
-                  </Link>
-              </span>
-            </li>
-          </ul>
-        </div>
-      </section>
-      <section className={styles.section}>
-        <DetailSectionHeader label='MAP' title='지도' />
-        <div className={styles.body}>
-          <KakaoMap lng={parseFloat(gpsX)} lat={parseFloat(gpsY)} />
-        </div>
-      </section>
-      <section className={styles.section}>
-        <DetailSectionHeader label='OTHERS' title='같은 분야의 행사' />
-        <Slider isFetching={isFetchingRealms} items={realmsQuery.body.items.item} />
-      </section>
+      <ScreenPaddingWrapper>
+        <section className={styles.section}>
+          <DetailSectionHeader label='DETAILS' title='상세 정보' />
+          <div className={styles.body}>
+            <ul className={styles.infoList}>
+              <li className={styles.item}>
+                <span className={styles.itemLabel}>
+                  <CiLocationOn className={styles.icon} />
+                  주소
+                </span>
+                <span className={styles.text}>{item.area} {item.place}</span>
+              </li>
+              <li className={styles.item}>
+                <span className={styles.itemLabel}>
+                  <PiCalendarMinusThin className={styles.icon} />
+                  기간
+                </span>
+                <span className={styles.text}>{formatDate(item.startDate)} ~ {formatDate(item.endDate)}</span>
+              </li>
+              <li className={styles.item}>
+                <span className={styles.itemLabel}>
+                  <LiaWonSignSolid className={styles.icon} />
+                  가격
+                </span>
+                <span className={styles.text}>{item.price.trim() || '홈페이지 확인'}</span>
+              </li>
+              <li className={styles.item}>
+                <span className={styles.itemLabel}>
+                  <PiPhoneCallLight className={styles.icon} />
+                  문의
+                </span>
+                <span className={styles.text}>{item.phone}</span>
+              </li>
+              <li className={styles.item}>
+                <span className={styles.itemLabel}>
+                  <RiHome2Line className={styles.icon} />
+                  홈페이지
+                </span>
+                <span className={styles.text}>
+                  <Link
+                    href={decodeHtml(item.url)}
+                    target='_blank'
+                    rel="noopener noreferrer"
+                    >
+                      {decodeHtml(item.url)}
+                    </Link>
+                </span>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className={styles.section}>
+          <DetailSectionHeader label='MAP' title='지도' />
+          <div className={styles.body}>
+            <KakaoMap lng={parseFloat(gpsX)} lat={parseFloat(gpsY)} />
+          </div>
+        </section>
+        <section className={styles.section}>
+          <DetailSectionHeader label='OTHERS' title='같은 분야의 행사' />
+          <Slider isFetching={isFetchingRealms} items={realmsQuery.body.items.item} />
+        </section>
+      </ScreenPaddingWrapper>
     </div>
   );
 };
