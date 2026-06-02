72d4ec3952174d065e4619b5f9e76dcd586ad6f5
Hyunsoo_kim <red_panda119@naver.com>
2026-06-03 00:28:44 +0900

    feat : 상수값 수정

--- src/constants/areas.ts
+++ src/constants/areas.ts
@@ -1,13 +1,13 @@
-export const AREAS = [
-  { code: '전체', name: '전체' },
-  { code: '서울', name: '서울' },
-  { code: '경기', name: '경기' },
-  { code: '강원', name: '강원' },
-  { code: '충북', name: '충북' },
-  { code: '충남', name: '충남' },
-  { code: '전북', name: '전북' },
-  { code: '전남', name: '전남' },
-  { code: '경북', name: '경북' },
-  { code: '경남', name: '경남' },
-  { code: '제주', name: '제주' }
-] as const;
+export const AREAS = {
+  '전체': '전체',
+  '서울': '서울',
+  '경기': '경기',
+  '강원': '강원',
+  '충북': '충북',
+  '충남': '충남',
+  '전북': '전북',
+  '전남': '전남',
+  '경북': '경북',
+  '경남': '경남',
+  '제주': '제주',
+} as const;
